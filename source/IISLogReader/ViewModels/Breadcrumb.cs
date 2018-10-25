ee71b4a9d2d2ebc1307d5e14d775973cb654b80d
Matt Salmon <matt@iua.co.za>
2018-10-25 21:47:15 +0200

    Added tabs for project view

---
+++ source/IISLogReader/ViewModels/Breadcrumb.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IISLogReader.ViewModels
+{
+    public class Breadcrumb
+    {
+        public Breadcrumb(string text) : this(text, null)
+        {
+
+        }
+        public Breadcrumb(string text, string url)
+        {
+            this.Text = text;
+            this.Url = url;
+        }
+
+        public string Text { get; set; }
+
+        public string Url { get; set; }
+    }
+}
